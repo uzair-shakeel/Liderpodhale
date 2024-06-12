5f87ea519b29e95f801b46d14fdcf532ff71247b
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-06-13 02:33:08 +0500

    Contact page completed

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,14 @@
+"use client";
 import React from "react";
-
-import { Button, ButtonGroup, Input } from "@nextui-org/react";
+import {
+  Button,
+  ButtonGroup,
+  Input,
+  Card,
+  CardBody,
+  Tab,
+  Tabs,
+} from "@nextui-org/react";
 import { FaLocationCrosshairs } from "react-icons/fa6";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
@@ -8,69 +16,134 @@ import { MdEmail } from "react-icons/md";
 const page = () => {
   return (
     <div className="py-12 px-[10%] bg-[#ecfafa]">
+      <h1 className="text-3xl text-center text-[#14a8e9] font-bold pb-8">
+        SKONTAKTUJ SIĘ Z NAMI
+      </h1>
       <div className="grid grid-cols-2 gap-12">
         <div className="pr-[12%]">
           <h2>Porozmawiajmy o wszystkim razem</h2>
-          <ButtonGroup className="py-6">
-            <Button className="bg-[#14a8e9]">Nowy Targ</Button>
-            <Button variant="bordered" className="border-[#14a8e9]">
-              Białka Tatrzańska
-            </Button>
-          </ButtonGroup>
-          <div className="mb-4">
-            <p>Jana Kilińskiego 15</p>
-            <Input
-              type="text"
-              placeholder=" 34-400 Nowy Targ"
-              size="lg"
-              color="primary"
-              className=" py-2"
-              startContent={
-                <FaLocationCrosshairs className="text-2xl text-default-400 pointer-events-none flex-shrink-0 " />
-              }
-            />
-          </div>
-          <div className="mb-4">
-            <p>Zadzwoń do nas</p>
-            <Input
-              type="number"
-              placeholder="+48 662 969 662"
-              size="lg"
-              color="primary"
-              className=" py-2"
-              startContent={
-                <FaPhoneAlt className="text-2xl text-default-400 pointer-events-none flex-shrink-0 py-[3px]" />
-              }
-            />
-          </div>
-          <div className="space-y-2">
-            <p>Pon - Pt, 8:00-17:00</p>
-            <p>E-mail </p>
-            <p>dział rejestracji i tłumaczeń</p>
-            <Input
-              type="email"
-              placeholder=" nowytarg@liderpodhale.pl"
-              size="lg"
-              color="primary"
-              className=" py-2"
-              startContent={
-                <MdEmail className="text-2xl text-default-400 pointer-events-none flex-shrink-0 " />
-              }
-            />
-            <p>dział ubezpieczeń</p>
-            <Input
-              type="email"
-              placeholder="ubezpieczenia@liderpodhale.pl"
-              size="lg"
-              color="primary"
-              className=" py-2"
-              startContent={
-                <MdEmail className="text-2xl text-default-400 pointer-events-none flex-shrink-0 " />
-              }
-            />
+          <div className="flex w-full flex-col">
+            <Tabs aria-label="Options" className="py-4">
+              <Tab key="photos" title="Nowy Targ">
+                <div>
+                  <div className="mb-4">
+                    <p>Jana Kilińskiego 15</p>
+                    <Input
+                      type="text"
+                      placeholder=" 34-400 Nowy Targ"
+                      size="lg"
+                      color="primary"
+                      className=" py-2"
+                      startContent={
+                        <FaLocationCrosshairs className="text-2xl text-default-400 pointer-events-none flex-shrink-0 " />
+                      }
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <p>Zadzwoń do nas</p>
+                    <Input
+                      type="number"
+                      placeholder="+48 662 969 662"
+                      size="lg"
+                      color="primary"
+                      className=" py-2"
+                      startContent={
+                        <FaPhoneAlt className="text-2xl text-default-400 pointer-events-none flex-shrink-0 py-[3px]" />
+                      }
+                    />
+                  </div>
+                  <div className="space-y-2">
+                    <p>Pon - Pt, 8:00-17:00</p>
+                    <p>E-mail </p>
+                    <p>dział rejestracji i tłumaczeń</p>
+                    <Input
+                      type="email"
+                      placeholder=" nowytarg@liderpodhale.pl"
+                      size="lg"
+                      color="primary"
+                      className=" py-2"
+                      startContent={
+                        <MdEmail className="text-2xl text-default-400 pointer-events-none flex-shrink-0 " />
+                      }
+                    />
+                    <p>dział ubezpieczeń</p>
+                    <Input
+                      type="email"
+                      placeholder="ubezpieczenia@liderpodhale.pl"
+                      size="lg"
+                      color="primary"
+                      className=" py-2"
+                      startContent={
+                        <MdEmail className="text-2xl text-default-400 pointer-events-none flex-shrink-0 " />
+                      }
+                    />
+                  </div>
+                </div>
+              </Tab>
+              <Tab key="music" title="Białka Tatrzańska">
+                <div>
+                  <div className="mb-4">
+                    <p>Środkowa 162</p>
+                    <Input
+                      type="text"
+                      placeholder="  34-405 Białka Tatrzańska"
+                      size="lg"
+                      color="primary"
+                      className=" py-2"
+                      startContent={
+                        <FaLocationCrosshairs className="text-2xl text-default-400 pointer-events-none flex-shrink-0 " />
+                      }
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <p>Zadzwoń do naszego innego biura</p>
+                    <Input
+                      type="number"
+                      placeholder="+48 887 770 774"
+                      size="lg"
+                      color="primary"
+                      className=" py-2"
+                      startContent={
+                        <FaPhoneAlt className="text-2xl text-default-400 pointer-events-none flex-shrink-0 py-[3px]" />
+                      }
+                    />
+                  </div>
+                  <div className="space-y-2">
+                    <p>Pon - Pt, 9:00-16:00</p>
+                    <p>E-mail </p>
+                    <Input
+                      type="email"
+                      placeholder=" nowytarg@liderpodhale.pl"
+                      size="lg"
+                      color="primary"
+                      className=" py-2"
+                      startContent={
+                        <MdEmail className="text-2xl text-default-400 pointer-events-none flex-shrink-0 " />
+                      }
+                    />
+                  </div>
+                </div>
+              </Tab>
+            </Tabs>
           </div>
         </div>
-        <div>Biiii</div>
+        <div className="px-[8%]">
+          <p>Interesuję się...</p>
+          <div className="flex flex-wrap gap-4 py-8">
+            <Button>Rejestracja samochodu</Button>
+            <Button>Ubezpieczenie</Button>
+            <Button>Tłumaczenie</Button>
+            <Button>Problem</Button>
+            <Button>Inne</Button>
+          </div>
+          <div className="space-y-6">
+            <Input type="text" label="Imię" color="primary" />
+            <Input type="text" label="Nazwisko" color="primary" />
+            <Input type="email" label="Adres email" color="primary" />
+            <Input type="number" label="Numer telefonu" color="primary" />
+            <Input type="text" label="Twoja wiadomość" color="primary" />
+          </div>
+        </div>
       </div>
     </div>
   );
