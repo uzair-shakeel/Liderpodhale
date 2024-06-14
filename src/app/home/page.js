ed20b89a5a4ea507d5d9a431c9643a4d75984467
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-06-14 17:09:33 +0500

    deployment issue

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -17,181 +17,189 @@ import { useLanguage } from "@/context/LanguageContext";
 
 const HomePage = () => {
   const { language } = useLanguage();
-  const itemsPolish = [
-    {
-      id: 1,
-      title: "Ubezpieczenie pojazdu",
-      description:
-        "W swojej ofercie posiadamy dwadzieścia towarzystw ubezpieczeniowych, co pozwala nam na przygotowanie idealnie dobranej oferty do potrzeb klienta.",
-      imgSrc: car,
-    },
-    {
-      id: 2,
-      title: "Ubezpieczenia majątkowe",
-      description:
-        "Twoje bezpieczeństwo jest dla nas najważniejsze! Od lat chronimy domy i majątek naszych klientów, stając się integralną częścią ich codziennego życia.",
-      imgSrc: property,
-    },
-    {
-      id: 3,
-      title: "Ubezpieczenia na życie",
-      description:
-        "Zabezpiecz siebie i swoją rodzinę przed niespodziewanymi wydarzeniami. Nasz doświadczony zespół pomoże ci w wyborze odpowiedniego ubezpieczenia na życie, dostosowanego do twoich indywidualnych potrzeb i celów.",
-      imgSrc: life,
-    },
-    {
-      id: 4,
-      title: "Ubezpieczenia firmowe",
-      description:
-        "Zapewniamy ochronę wobec różnych ryzyk związanych z prowadzeniem działalności gospodarczej. Dzięki nam obejmiesz ochroną różne aspekty twojej działalności, takie jak ubezpieczenie mienia i mienia firmy przed zniszczeniem, kradzieżą czy pożarem.",
-      imgSrc: company,
-    },
-    {
-      id: 5,
-      title: "Ubezpieczenia podróżne",
-      description:
-        "Nasze ubezpieczenia podróżne są dostosowane do indywidualnych potrzeb podróżującego, uwzględniając cel i rodzaj podróży. Pomożemy ci w pełni cieszyć się podróżą, mając pewność, że jesteś dobrze zabezpieczony na wypadek nieprzewidzianych sytuacji.",
-      imgSrc: travel,
-    },
-    {
-      id: 6,
-      title: "Ubezpieczenia szkolne",
-      description:
-        "Zapewnimy ochronę w przypadku nieszczęśliwych wypadków dla twoich pociech. Nasze ubezpieczenia szkolne są stworzone specjalnie dla uczniów oraz dzieci przedszkolnych.",
-      imgSrc: school,
-    },
-  ];
+  const itemsPolish =
+    [
+      {
+        id: 1,
+        title: "Ubezpieczenie pojazdu",
+        description:
+          "W swojej ofercie posiadamy dwadzieścia towarzystw ubezpieczeniowych, co pozwala nam na przygotowanie idealnie dobranej oferty do potrzeb klienta.",
+        imgSrc: car,
+      },
+      {
+        id: 2,
+        title: "Ubezpieczenia majątkowe",
+        description:
+          "Twoje bezpieczeństwo jest dla nas najważniejsze! Od lat chronimy domy i majątek naszych klientów, stając się integralną częścią ich codziennego życia.",
+        imgSrc: property,
+      },
+      {
+        id: 3,
+        title: "Ubezpieczenia na życie",
+        description:
+          "Zabezpiecz siebie i swoją rodzinę przed niespodziewanymi wydarzeniami. Nasz doświadczony zespół pomoże ci w wyborze odpowiedniego ubezpieczenia na życie, dostosowanego do twoich indywidualnych potrzeb i celów.",
+        imgSrc: life,
+      },
+      {
+        id: 4,
+        title: "Ubezpieczenia firmowe",
+        description:
+          "Zapewniamy ochronę wobec różnych ryzyk związanych z prowadzeniem działalności gospodarczej. Dzięki nam obejmiesz ochroną różne aspekty twojej działalności, takie jak ubezpieczenie mienia i mienia firmy przed zniszczeniem, kradzieżą czy pożarem.",
+        imgSrc: company,
+      },
+      {
+        id: 5,
+        title: "Ubezpieczenia podróżne",
+        description:
+          "Nasze ubezpieczenia podróżne są dostosowane do indywidualnych potrzeb podróżującego, uwzględniając cel i rodzaj podróży. Pomożemy ci w pełni cieszyć się podróżą, mając pewność, że jesteś dobrze zabezpieczony na wypadek nieprzewidzianych sytuacji.",
+        imgSrc: travel,
+      },
+      {
+        id: 6,
+        title: "Ubezpieczenia szkolne",
+        description:
+          "Zapewnimy ochronę w przypadku nieszczęśliwych wypadków dla twoich pociech. Nasze ubezpieczenia szkolne są stworzone specjalnie dla uczniów oraz dzieci przedszkolnych.",
+        imgSrc: school,
+      },
+    ] || [];
 
-  const itemsUkrainian = [
-    {
-      id: 1,
-      title: "Страхування транспортних засобів",
-      description:
-        "У нашій пропозиції є двадцять страхових компаній, що дозволяє нам підготувати ідеально підібрану пропозицію відповідно до потреб клієнта.",
-      imgSrc: car,
-    },
-    {
-      id: 2,
-      title: "Майнове страхування",
-      description:
-        "Ваша безпека є для нас найважливішою! Протягом багатьох років ми захищаємо будинки та майно наших клієнтів, стаючи невід'ємною частиною їхнього повсякденного життя.",
-      imgSrc: property,
-    },
-    {
-      id: 3,
-      title: "Страхування життя",
-      description:
-        "Забезпечте себе та свою родину від непередбачених подій. Наша досвідчена команда допоможе вам вибрати відповідне страхування життя, адаптоване до ваших індивідуальних потреб і цілей.",
-      imgSrc: life,
-    },
-    {
-      id: 4,
-      title: "Корпоративне страхування",
-      description:
-        "Ми забезпечуємо захист від різних ризиків, пов'язаних з веденням бізнесу. Завдяки нам ви покриєте захистом різні аспекти вашої діяльності, такі як страхування майна та майна компанії від руйнування, крадіжки чи пожежі.",
-      imgSrc: company,
-    },
-    {
-      id: 5,
-      title: "Туристичне страхування",
-      description:
-        "Наші туристичні страхування адаптовані до індивідуальних потреб мандрівника, з урахуванням мети та типу подорожі. Ми допоможемо вам повністю насолодитися подорожжю, будучи впевненими, що ви добре захищені у разі непередбачених ситуацій.",
-      imgSrc: travel,
-    },
-    {
-      id: 6,
-      title: "Шкільне страхування",
-      description:
-        "Ми забезпечимо захист у разі нещасних випадків для ваших дітей. Наші шкільні страхування створені спеціально для учнів та дітей дошкільного віку.",
-      imgSrc: school,
-    },
-  ];
+  const itemsUkrainian =
+    [
+      {
+        id: 1,
+        title: "Страхування транспортних засобів",
+        description:
+          "У нашій пропозиції є двадцять страхових компаній, що дозволяє нам підготувати ідеально підібрану пропозицію відповідно до потреб клієнта.",
+        imgSrc: car,
+      },
+      {
+        id: 2,
+        title: "Майнове страхування",
+        description:
+          "Ваша безпека є для нас найважливішою! Протягом багатьох років ми захищаємо будинки та майно наших клієнтів, стаючи невід'ємною частиною їхнього повсякденного життя.",
+        imgSrc: property,
+      },
+      {
+        id: 3,
+        title: "Страхування життя",
+        description:
+          "Забезпечте себе та свою родину від непередбачених подій. Наша досвідчена команда допоможе вам вибрати відповідне страхування життя, адаптоване до ваших індивідуальних потреб і цілей.",
+        imgSrc: life,
+      },
+      {
+        id: 4,
+        title: "Корпоративне страхування",
+        description:
+          "Ми забезпечуємо захист від різних ризиків, пов'язаних з веденням бізнесу. Завдяки нам ви покриєте захистом різні аспекти вашої діяльності, такі як страхування майна та майна компанії від руйнування, крадіжки чи пожежі.",
+        imgSrc: company,
+      },
+      {
+        id: 5,
+        title: "Туристичне страхування",
+        description:
+          "Наші туристичні страхування адаптовані до індивідуальних потреб мандрівника, з урахуванням мети та типу подорожі. Ми допоможемо вам повністю насолодитися подорожжю, будучи впевненими, що ви добре захищені у разі непередбачених ситуацій.",
+        imgSrc: travel,
+      },
+      {
+        id: 6,
+        title: "Шкільне страхування",
+        description:
+          "Ми забезпечимо захист у разі нещасних випадків для ваших дітей. Наші шкільні страхування створені спеціально для учнів та дітей дошкільного віку.",
+        imgSrc: school,
+      },
+    ] || [];
 
-  const cardDataPolish = [
-    {
-      imageSrc:
-        "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5308_cropped.JPG",
-      footerText: "Ubezpieczenia",
-      linkText: "insurance",
-    },
-    {
-      imageSrc:
-        "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5192_cropped.JPG",
-      footerText: "Tłumaczenie",
-      linkText: "translation",
-    },
-    {
-      imageSrc:
-        "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5246_cropped.JPG",
-      footerText: "Rejestracja pojazdów",
-      linkText: "registration",
-    },
-  ];
+  const cardDataPolish =
+    [
+      {
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5308_cropped.JPG",
+        footerText: "Ubezpieczenia",
+        linkText: "insurance",
+      },
+      {
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5192_cropped.JPG",
+        footerText: "Tłumaczenie",
+        linkText: "translation",
+      },
+      {
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5246_cropped.JPG",
+        footerText: "Rejestracja pojazdów",
+        linkText: "registration",
+      },
+    ] || [];
 
-  const cardDataUkrainian = [
-    {
-      imageSrc:
-        "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5308_cropped.JPG",
-      footerText: "Страхування",
-      linkText: "insurance",
-    },
-    {
-      imageSrc:
-        "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5192_cropped.JPG",
-      footerText: "Переклад",
-      linkText: "translation",
-    },
-    {
-      imageSrc:
-        "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5246_cropped.JPG",
-      footerText: "Реєстрація транспортних засобів",
-      linkText: "registration",
-    },
-  ];
+  const cardDataUkrainian =
+    [
+      {
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5308_cropped.JPG",
+        footerText: "Страхування",
+        linkText: "insurance",
+      },
+      {
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5192_cropped.JPG",
+        footerText: "Переклад",
+        linkText: "translation",
+      },
+      {
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/img/home-intro/IMG_5246_cropped.JPG",
+        footerText: "Реєстрація транспортних засобів",
+        linkText: "registration",
+      },
+    ] || [];
 
-  const testimonialsPolish = [
-    {
-      companyName: "KALATA",
-      testimonialText:
-        "Korzystając z usług Multiagencji Lider, zapewniliśmy najlepsze ubezpieczenie dla naszych uczniów nauki jazdy i całej floty transportowej. Ich doświadczenie w branży jest dla nas nieocenione. Z całego serca ich polecamy!",
-      imageSrc: "https://luxury-bunny-b94631.netlify.app/assets/kalata.jpg",
-    },
-    {
-      companyName: "RAFRACING",
-      testimonialText:
-        "Jako sklep motocyklowy, każdy detal jest dla nas ważny. Z pomocą Grzesia z biura Lider, doskonale dostosowaliśmy nasze ubezpieczenie, zapewniając ochronę naszym klientom i produktom. Dziękujemy za współpracę!",
-      imageSrc: "https://luxury-bunny-b94631.netlify.app/assets/rafracing.jpg",
-    },
-    {
-      companyName: "Grzegorczyk Hale Stalowe",
-      testimonialText:
-        "Dzięki wsparciu biura Lider, nasze hale stalowe są doskonale ubezpieczone. Profesjonalne porady i zaangażowanie Grzesia pozwalają nam skupić się na naszej głównej działalności. Dziękujemy za współpracę!",
-      imageSrc:
-        "https://luxury-bunny-b94631.netlify.app/assets/testimonials1.jpg",
-    },
-  ];
+  const testimonialsPolish =
+    [
+      {
+        companyName: "KALATA",
+        testimonialText:
+          "Korzystając z usług Multiagencji Lider, zapewniliśmy najlepsze ubezpieczenie dla naszych uczniów nauki jazdy i całej floty transportowej. Ich doświadczenie w branży jest dla nas nieocenione. Z całego serca ich polecamy!",
+        imageSrc: "https://luxury-bunny-b94631.netlify.app/assets/kalata.jpg",
+      },
+      {
+        companyName: "RAFRACING",
+        testimonialText:
+          "Jako sklep motocyklowy, każdy detal jest dla nas ważny. Z pomocą Grzesia z biura Lider, doskonale dostosowaliśmy nasze ubezpieczenie, zapewniając ochronę naszym klientom i produktom. Dziękujemy za współpracę!",
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/rafracing.jpg",
+      },
+      {
+        companyName: "Grzegorczyk Hale Stalowe",
+        testimonialText:
+          "Dzięki wsparciu biura Lider, nasze hale stalowe są doskonale ubezpieczone. Profesjonalne porady i zaangażowanie Grzesia pozwalają nam skupić się na naszej głównej działalności. Dziękujemy za współpracę!",
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/testimonials1.jpg",
+      },
+    ] || [];
 
-  const testimonialsUkrainian = [
-    {
-      companyName: "КАЛАТА",
-      testimonialText:
-        "Використовуючи послуги Multiagencji Lider, ми забезпечили найкраще страхування для наших учнів водіння та всього транспортного парку. Їхній досвід у галузі для нас безцінний. Щиро рекомендуємо їх!",
-      imageSrc: "https://luxury-bunny-b94631.netlify.app/assets/kalata.jpg",
-    },
-    {
-      companyName: "РАФРЕЙСИНГ",
-      testimonialText:
-        "Як магазин мотоциклів, кожна деталь для нас важлива. Завдяки допомозі Гжеся з офісу Lider, ми чудово налаштували наше страхування, забезпечивши захист нашим клієнтам і продуктам. Дякуємо за співпрацю!",
-      imageSrc: "https://luxury-bunny-b94631.netlify.app/assets/rafracing.jpg",
-    },
-    {
-      companyName: "Grzegorczyk Hale Stalowe",
-      testimonialText:
-        "Завдяки підтримці офісу Lider, наші сталеві зали чудово застраховані. Професійні поради та залучення Гжеся дозволяють нам зосередитися на нашій основній діяльності. Дякуємо за співпрацю!",
-      imageSrc:
-        "https://luxury-bunny-b94631.netlify.app/assets/testimonials1.jpg",
-    },
-  ];
+  const testimonialsUkrainian =
+    [
+      {
+        companyName: "КАЛАТА",
+        testimonialText:
+          "Використовуючи послуги Multiagencji Lider, ми забезпечили найкраще страхування для наших учнів водіння та всього транспортного парку. Їхній досвід у галузі для нас безцінний. Щиро рекомендуємо їх!",
+        imageSrc: "https://luxury-bunny-b94631.netlify.app/assets/kalata.jpg",
+      },
+      {
+        companyName: "РАФРЕЙСИНГ",
+        testimonialText:
+          "Як магазин мотоциклів, кожна деталь для нас важлива. Завдяки допомозі Гжеся з офісу Lider, ми чудово налаштували наше страхування, забезпечивши захист нашим клієнтам і продуктам. Дякуємо за співпрацю!",
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/rafracing.jpg",
+      },
+      {
+        companyName: "Grzegorczyk Hale Stalowe",
+        testimonialText:
+          "Завдяки підтримці офісу Lider, наші сталеві зали чудово застраховані. Професійні поради та залучення Гжеся дозволяють нам зосередитися на нашій основній діяльності. Дякуємо за співпрацю!",
+        imageSrc:
+          "https://luxury-bunny-b94631.netlify.app/assets/testimonials1.jpg",
+      },
+    ] || [];
 
   const carouselItems = language === "pl" ? itemsPolish : itemsUkrainian;
   // const carouselItems = itemsUkrainian;
@@ -302,7 +310,7 @@ const HomePage = () => {
           {language === "pl" ? `NASZE USŁUGI` : `НАШІ ПОСЛУГИ`}
         </h3>
         <div className="grid lg:grid-cols-3 gap-10">
-          {cardData.map((card, index) => (
+          {cardData?.map((card, index) => (
             <Card
               key={index}
               imageSrc={card.imageSrc}
@@ -326,7 +334,7 @@ const HomePage = () => {
         </p>
 
         <div className="flex gap-[20px] my-[20px] flex-col md:flex-row">
-          {testimonials.slice(0, 2).map((testimonial, index) => (
+          {testimonials?.slice(0, 2).map((testimonial, index) => (
             <Check
               key={index}
               companyName={testimonial.companyName}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HomePage from "./home/page";
 
 export default function Home() {
